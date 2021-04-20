87bf814071edff1433f679cdb5edb15373a32172
brunobbr <bruno-loll@hotmail.com>
2021-04-20 10:36:03 -0400

    fDay

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express, { request, response } from 'express';
+
+const app = express();
+
+
+
+
+
+
+
+app.get("/", (request, response) => {
+    return response.json({
+        message: "HELLO WORLD",
+    });
+});
+app.post("", (request, response) => {
+    
+})
+app.listen(3333, () => console.log("Server is running"));
