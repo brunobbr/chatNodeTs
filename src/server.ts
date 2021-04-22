98c64eae11fd0362cf5c797c95a4b10e524bc343
brunobbr <bruno-loll@hotmail.com>
2021-04-22 16:59:59 -0400

    fronEnd 1/2

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,4 @@
-import express, { request, response } from 'express';
-import "./database";
-import { routes } from "./routes";
-const app = express();
-app.use(express.json());
-app.use(routes);
-app.listen(3333, () => console.log("Server is running"));
+import {http} from "./http";
+import "./websocket/client";
+
+http.listen(3333, () => console.log("Server is running"));
---
+++ src/websocket/client.ts
@@ -0,0 +1,6 @@
+import {io} from "../http";
+io.on("connect", (socket) => {
+    socket.on("client_first_access", params =>{
+       console.log(params); 
+    })
+});
