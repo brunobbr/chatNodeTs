c2389113c3dfeaf74ca28be4fd1cde73e9b3733c
brunobbr <bruno-loll@hotmail.com>
2021-04-24 20:03:37 -0400

    chat histórico

--- src/http.ts
+++ src/http.ts
@@ -13,13 +13,16 @@ app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
 app.get("/pages/client", (request, response) => {
-    return response.render("html/client.html")
-})
+    return response.render("html/client.html");
+});
+app.get("/pages/admin", (request, response) => {
+    return response.render("html/admin.html");
+});
 const http = createServer(app);
 const io = new Server(http);
 io.on("connection", (socket: Socket) => {
     /* console.log("Se conectou", socket.id); */
-})
+});
 app.use(express.json());
 app.use(routes);
 
